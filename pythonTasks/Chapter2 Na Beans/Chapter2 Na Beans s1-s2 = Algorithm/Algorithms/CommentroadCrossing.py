ef2c9abf8d8dfbdabf2eb4575d564e7f6804dad5
QueenColly <colleteumeh0@gmail.com>
2026-04-13 09:58:38 +0100

    first commit

---
+++ pythonTasks/Chapter2 Na Beans/Chapter2 Na Beans s1-s2 = Algorithm/Algorithms/CommentroadCrossing.py
@@ -0,0 +1,14 @@
+SAFEST WAY TO CROSS THE ROAD
+
+if  you want  to cross the road safely , you must ensure that you're an adult who is familiar with this activity
+else look for an adult to assist you cross 
+
+As an adult , you need to observe the kind of road you're crossing
+
+make sure the traffic light is red boefore taking a step except you're in a hurry
+, then we do this
+
+look left and look right , 
+
+
+

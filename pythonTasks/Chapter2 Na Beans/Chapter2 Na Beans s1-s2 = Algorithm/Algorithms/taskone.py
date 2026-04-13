b8d28f9c1c53c04c163d831992b0fea514a957f6
QueenColly <colleteumeh0@gmail.com>
2026-04-13 08:18:28 +0100

    first commit

---
+++ pythonTasks/Chapter2 Na Beans/Chapter2 Na Beans s1-s2 = Algorithm/Algorithms/taskone.py
@@ -0,0 +1,28 @@
+MAKING A CUP OF TEA STEPS ;
+
+Get a kettle from your kitchen cabinet and wash in a kitchen sink or a big bowl ,
+
+Rinse your kettle afterwards ,
+
+then fill it up with water from your tap or water store .
+
+Get a lighter or matchstick after you must have ensured that your gas cooker is properly turned on and light it up .
+
+Then , place your kettle on your gas cooker and let your water heat up for about 10 to 15 minutes.
+
+Note that this all depends on the quantity of tea you want to make . 
+
+if you have to boil a water that needs you to make about 5 cups of tea ,measure 1.25 liters of water with your measuring cup 
+
+else measure 250-280 ml of waters for just a cup of tea
+
+put a tea bag (powder chocolate )  in a clean cup and a spoon to stir after you wash in a kitchen sink 
+
+for sweetener , add sugar or honey 
+
+
+
+
+
+
+

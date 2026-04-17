93bc8b6f5e62434d2e844f25c798a523adfb1504
QueenColly <colleteumeh0@gmail.com>
2026-04-17 10:17:09 +0100

    first commit

---
+++ pythonTasks/ClassWork/swap.py
@@ -0,0 +1,28 @@
+#a = 5 
+#b = 10
+
+first_number = 5
+second_number = 10
+third_number = first_number
+first_number = second_number
+second_number = third_number
+
+
+print(first_number, + second_number)
+
+#a = 5
+#b = 10 
+#temp = a 
+#a = b
+#b = temp
+#print(a ,  b)
+#
+
+#a = 5
+#b = 10
+#a,b = b, a
+#
+#print(a,b)
+#
+
+#a student wants to swap a = 5 and b = 1= so that a becomes 10 and b becomes 5 . They write : a = b; b = a .  Explain the bug . Write the correct solution
---
+++ pythonTasks/ClassWork/tiered_discount.py
@@ -0,0 +1,13 @@
+#Design a program that applies tiered discounts based on customer's total spending in a store. The greater purchase amount, the higher the discount offered.
+#
+#
+#purchases between 1000 and 10000 receive a 5% discount
+#purchases between 10000 and 50000 receive  a 10 % discount
+#purchases above 50000 receive a 20% discount.
+#
+#Ensure the program calculates and displays the appropriate discount for the given amount.
+
+
+    int first_purchase = 5% 
+    int secont purchase = 10% 
+    int third_purchase =  20%   

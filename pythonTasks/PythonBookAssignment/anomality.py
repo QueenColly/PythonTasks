1cbe78b5ed3e7938217925ed9d763801b4d87575
QueenColly <colleteumeh0@gmail.com>
2026-04-21 10:29:53 +0100

    first commit

---
+++ pythonTasks/PythonBookAssignment/anomality.py
@@ -0,0 +1,10 @@
+#3.2
+
+#a = b = 7
+#print('a=', a, '\nb =', b)
+#
+
+#this code prints a = 7 
+#                 b = 7
+#
+# both have thesame value and as such, they are equated to 7 and runs as it is stated. Therefore , there's nothing wrong with this code
---
+++ pythonTasks/PythonBookAssignment/print_code.py
@@ -0,0 +1,9 @@
+#3.3
+
+#for row in range(10):
+#    for column in range (10):
+#        print('<' if row % 2 == 1 else '>', end='')
+#    print()
+#
+#
+#it prints less than(<) in the odd in row 1,3,5,7,9 and prints greater than(>) in row 2,4,6,8,10
